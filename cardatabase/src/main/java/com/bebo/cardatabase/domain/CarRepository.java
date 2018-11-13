6295b371164945fbc891b652201d568e2ef15d65
Bebo Elhosary <beboelhosary@hotmail.com>
2018-11-13 17:22:35 -0500

    Add Spring Data REST and access to REStful stuff at /api

--- cardatabase/src/main/java/com/bebo/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/bebo/cardatabase/domain/CarRepository.java
@@ -2,10 +2,9 @@ package com.bebo.cardatabase.domain;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // CrudRepository provides multiple CRUD methods:
 // long count() - returns number of entities
@@ -14,14 +13,14 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 // Optional<T> findById(ID Id) - returns 1 item by id
 // and many more!
 
-
+@RepositoryRestResource // this annotation allows us to include custom queries
 public interface CarRepository extends CrudRepository <Car, Long> {
 
 	// Fetch cars by brand
-	List<Car> findByBrand(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
 	
 	// Fetch cars by brand
-	List<Car> findByColor(String color);
+	List<Car> findByColor(@Param("color") String color);
 		
 	// Fetch cars by brand
 	List<Car> findByYear(int year);
