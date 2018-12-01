e2a12f2cd165f454d22eb47f98df0e9432199512
Bebo Elhosary <beboelhosary@hotmail.com>
2018-12-01 17:35:22 -0500

    Add Spring Security code. JWT error

---
+++ cardatabase/src/main/java/com/bebo/cardatabase/domain/AccountCredentials.java
@@ -0,0 +1,22 @@
+package com.bebo.cardatabase.domain;
+// dont need @Entity for credentials b/c we dont have to 
+// save credentials to the database
+public class AccountCredentials {
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
